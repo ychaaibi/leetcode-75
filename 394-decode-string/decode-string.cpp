105c4c2efb8c4ee4aa2601d5390d55298e7c57d3
Youness Chaaibi <89740067+ychaaibi@users.noreply.github.com>
2024-04-25 15:38:06 +0100

    Time: 4 ms (16.05%) | Memory: 7.7 MB (82.83%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    string decodeString(string s) {
+        stack<char> st;
+        
+        for ( int i=0; i<s.length(); i++ )
+        {
+            if ( s[i] != ']' )
+                st.push( s[i] );
+            else
+            {
+                string curr;
+                
+                curr.reserve(1000);
+                
+                while ( st.top() != '[')
+                {
+                    curr.push_back( st.top() );
+                    st.pop();
+                }
+                
+                reverse(curr.begin(), curr.end());
+                st.pop();
+                
+                int num = 0, mult = 1;
+
+                while ( !st.empty() and isdigit( st.top() ) )
+                {
+                    num += ( ( st.top() - '0' ) * mult );
+                    mult *= 10;
+                    st.pop();
+                }
+                
+                for ( int k=0; k<num; k++ )
+                    for ( int j=0; j<curr.length(); j++ )
+                        st.push( curr[j] );
+            }
+        }
+        
+        string ans( st.size(), ' ' );
+        
+        for ( int i=ans.size() - 1; i>=0; i-- )
+        {
+            ans[i] = st.top();
+            st.pop();
+        }
+        
+        return ( ans );
+    }
+};
