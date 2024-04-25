5f0513607676c838291094c790cdbe22fcc6711c
Youness Chaaibi <89740067+ychaaibi@users.noreply.github.com>
2024-04-26 00:37:03 +0100

    Time: 2 ms (40.38%) | Memory: 9.3 MB (33.71%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp( nums.size( ) );
+
+        if ( nums.size() == 1 )
+            return ( nums[0] );
+
+        dp[0] = nums[0], dp[1] = nums[1];
+
+        for ( int i=2; i<nums.size(); i++ )
+        {
+            dp[i] = nums[i];
+
+            for ( int j=i - 2; j>=0; j-- )
+                dp[i] = max( dp[i], nums[i] + dp[j] );
+        }
+
+        return ( max( dp[dp.size() - 1], dp[dp.size() - 2] ) );
+    }
+};
