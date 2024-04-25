991ad278456eb578b0404aa791fa1ec129148e05
Youness Chaaibi <89740067+ychaaibi@users.noreply.github.com>
2024-04-25 14:09:23 +0100

    Time: 6 ms (28.29%) | Memory: 11.2 MB (38.49%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>  count;
+        unordered_map<int,int>  hash;
+        
+        for ( int i=0; i<arr.size(); i++ )
+            hash[arr[i]]++;
+        
+        for ( auto& [k, v] : hash )
+        {
+            count[v]++;
+            if ( count[v] > 1 )
+                return ( false );
+        }
+        
+        return ( true );
+    }
+};
