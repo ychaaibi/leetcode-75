1bcf78779ebb8dc783769fcd301ad06cc0cbd585
Youness Chaaibi <89740067+ychaaibi@users.noreply.github.com>
2024-04-26 01:55:48 +0100

    Time: 124 ms (49.16%) | Memory: 105.4 MB (37.27%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         vector<int> ans(temperatures.size(), 0);
-
         stack<int>  st;
 
         for ( int i=0; i<temperatures.size(); i++ )
@@ -19,7 +18,7 @@ public:
                 st.push( i );
             }
         }
-
+        
         return ( ans );
     }
 };
