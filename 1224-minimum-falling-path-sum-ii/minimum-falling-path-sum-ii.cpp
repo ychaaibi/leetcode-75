7ac70f43f05a91c5d593cca5795703932883bf57
Youness Chaaibi <89740067+ychaaibi@users.noreply.github.com>
2024-04-26 20:53:28 +0100

    Time: 310 ms (36.38%) | Memory: 18.3 MB (34.63%) - LeetSync

---
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        vector<vector<int>> dp( grid.size(), vector<int>(grid.size(), INT_MAX) );
+
+        dp.front() = grid.front();
+
+        for ( int i=1; i<grid.size(); i++ )
+        {
+            for ( int j=0; j<grid[0].size(); j++ )
+            {
+                for ( int _j=0; _j<grid[0].size(); _j++ )
+                {
+                    if ( j == _j )
+                        continue ;
+                    dp[i][j] = min( dp[i][j], dp[i - 1][_j] + grid[i][j] );
+                }
+            }
+        }
+
+        int ans = INT_MAX;
+        
+        for ( int j=0; j<grid[0].size(); j++ )
+            ans = min( ans, dp.back()[j] );
+
+        return ( ans );
+    }
+};
